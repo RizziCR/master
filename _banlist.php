b4ffd0e14032d2adbece11bbd68bc38f5f1c4037
Sawascwoolf <admin@forumkl.de>
2014-11-19 10:00:21 +0100

    Register Globals nicht mehr notwendig für _banlist.php + Performance durch Verwendung von in_array statt for Schleife

--- _banlist.php
+++ _banlist.php
@@ -2,7 +2,6 @@
 	$banlist = array(	"62.67.235.169", // www.felixbruns.de
 						"212.112.227.114"); // www.unmatrix.org & www.etstec.de
 
-	for ($i=0;$i<count($banlist);$i++)
-		if ($REMOTE_ADDR == $banlist[$i])
+	if (in_array($_SERVER["REMOTE_ADDR"],$banlist))
 			die();
 ?>
